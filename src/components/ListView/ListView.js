96536d17fbee43404b277ae842c58f4a24721f6a
Ana Bennett <anabennett1111@gmail.com>
2022-11-01 10:00:04 -0500

    add ul element to listview and classname for styling

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import './ListView.css';
 
-const ListView = ({articleDetails }) => {
-    let sortedArticles = articleDetails.sort((a, b) => 
+const ListView = ({ articleDetails }) => {
+    let sortedArticles = articleDetails.sort((a, b) =>
         (a.section > b.section) ? 1 : ((b.section > a.section) ? -1 : 0))
 
-    const mappedArticles = sortedArticles.map(article =>{
+    const mappedArticles = sortedArticles.map(article => {
         return (
-        <div key={article.title}>
-          <h3>{article.title}</h3> {/* have an onclick, save that article title to a state, use router 5 to navigate to another page , then a filter of some kind to find that article or a find within that api and direct it to that story then load the rest of the story */}
-         <p>{article.abstract}</p>
-        </div>
+            <div key={article.title}>
+                <ul className='list'>
+                   <p>{article.section}</p>
+                   <h3>{article.title}</h3> {/* have an onclick, save that article title to a state, use router 5 to navigate to another page , then a filter of some kind to find that article or a find within that api and direct it to that story then load the rest of the story */}
+                   <p>{article.abstract}</p>
+                </ul>
+            </div>
         )
-}) 
+    })
 
 
- return (
-    <div>
-        <h1>List of Articles with their Abstracts</h1>
-        <section>{mappedArticles}</section>
-
-    </div>
- )
+    return (
+        <div className='listView'>
+            <h1>Top Stories sorted by Section</h1>
+            <section>
+                {mappedArticles}
+            </section>
+        </div>
+    )
 }
 
 export default ListView;
